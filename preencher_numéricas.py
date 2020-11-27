c19559fa9c95c26f93aa9c645b9c0888cfb87f3d
Douglas Cardoso <douglascardoso@usp.br>
2020-11-27 16:42:01 -0300

    Delete preencher_numéricas.py

--- preencher_numéricas.py
+++
@@ -1,7 +0,0 @@
-from fancyimpute import KNN
-KNN_imputer = KNN()
-
-num_features = ['cod_municipio', 'feature_16', 'feature_17', 'feature_13', 'feature_14', 'feature_15', 'feature_18','feature_04', 'feature_06', 'feature_07', 'feature_09', 'feature_10' ]
-df_knn = cenarios.copy()
-df_knn = KNN_imputer.fit_transform(df_knn[num_features])
-df_knn
